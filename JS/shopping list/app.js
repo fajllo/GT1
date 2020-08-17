ebbb621c307d501687de9cf7f4f980285c0b11f0
fsdr <filipsordyl8@gmail.com>
2020-08-17 21:51:11 +0200

    empty sting do nothing

--- JS/shopping list/app.js
+++ JS/shopping list/app.js
@@ -2,6 +2,7 @@ const input = document.querySelector("#add");
 const ol =document.querySelector("#lista");
 input.addEventListener('keypress',function (e){
   if (e.keyCode === 13){
+    if (!this.value) return;
       const newItem = this.value;
       const newLi = document.createElement('li');
       newLi.innerText = newItem;
