f488fe5e43b05294d3589ca69f86a6bea6bc8006
fsdr <filipsordyl8@gmail.com>
2020-08-18 20:01:36 +0200

    co to sie podziało

--- JS/getCoinGame/app.js
+++ JS/getCoinGame/app.js
@@ -9,3 +9,30 @@ function isTouching(a, b) {
 		aRect.left > bRect.left + bRect.width
 	);
 }
+
+const player = document.getElementById("player");
+window.addEventListener("keyup",function (e){
+	if(e.keyCode === 40){
+		const topPo = posit(player.style.top);
+		player.style.top = `${topPo + 25}px`;
+	}
+	else if(e.keyCode === 38){
+		const topPo = posit(player.style.top);
+		player.style.top = `${topPo - 25}px`;
+	}
+	else if(e.keyCode === 39){
+		const leftPo = posit(player.style.left);
+		player.style.transform = 'scale(1,1)';
+		player.style.left = `${leftPo + 25}px`;
+	}else if(e.keyCode === 37){
+		const leftPo = posit(player.style.left);
+		player.style.transform = 'scale(-1,1)';
+		player.style.left = `${leftPo - 25}px`;
+	}
+})
+
+const posit = (pos) =>{
+	if (!pos) return 10;
+	 return parseInt(pos.slice(0,-2));
+
+};
