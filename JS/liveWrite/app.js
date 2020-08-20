01b4ae5554d596c6f61148cdac41e719ae420175
fsdr <filipsordyl8@gmail.com>
2020-08-20 14:44:59 +0200

    e.target = this

--- JS/liveWrite/app.js
+++ JS/liveWrite/app.js
@@ -1,9 +1,10 @@
 const input1 = document.querySelector("#input1");
-const h1 = document.querySelector("h1");
+const h1 = document.querySelector("#input2");
 
 
 
 input1.addEventListener('input', function (e){
-    h1.innerText = e.target.value;
+    input2.innerText = this.value;
+    
 
 });
