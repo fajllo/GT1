e97f6a755c79ed458042f429faa30186d9735417
fsdr <filipsordyl8@unilever.com>
2020-08-21 15:55:02 +0200

    fetch with function

--- JS/httpRequest/xml/fetch/app.js
+++ JS/httpRequest/xml/fetch/app.js
@@ -3,33 +3,64 @@ const fetchPlanets = (url= "https://swapi.dev/api/planets/") => {
     return fetch(url)
 }
 
+const statsAndParse = (response) => {
+    if (!response.ok){
+        throw new Error("wrrrrrrrrrr!");
+    }
+    return response.json();
+    
+}
+const printPlanet = (data) => {
+    console.log(data)
+    for(let planet of data.results){
+        console.log(planet.name)
+    }
+    return Promise.resolve(data.next)
+}
 
-fetch("https://swapi.dev/api/planets/")
-    .then((response) =>{
-        if (!response.ok){
-            throw new Error("wrrrrrrrrrr!");
-        }
-        return response.json();
-    })
-    .then((data) => {
-        const film = data.results[0].films[0];
-        return fetch(film)
-     })
-     .then((response) => {
-        if (!response.ok){
-            throw new Error("wrrrrrrrrrr!");
-        }
-        return response.json();
 
-     })
-     .then((data) => {
-         console.log(data.title);
-     })
-    .catch((err) => {
-        console.log(err)
-    });
 
-    const x = [1,2,3,4];
+fetchPlanets()
+.then(statsAndParse)
+.then(printPlanet)
+.then(fetchPlanets)
+.then(statsAndParse)
+.then(printPlanet)
+.then(fetchPlanets)
+.then(statsAndParse)
+.then(printPlanet)
+.catch(err => {
+    console.log(err)
+})
 
-    const [a,b] = x;
-    console.log(a, b)
+
+
+// fetch("https://swapi.dev/api/planets/")
+//     .then((response) =>{
+//         if (!response.ok){
+//             throw new Error("wrrrrrrrrrr!");
+//         }
+//         return response.json();
+//     })
+//     .then((data) => {
+//         const film = data.results[0].films[0];
+//         return fetch(film)
+//      })
+//      .then((response) => {
+//         if (!response.ok){
+//             throw new Error("wrrrrrrrrrr!");
+//         }
+//         return response.json();
+
+//      })
+//      .then((data) => {
+//          console.log(data.title);
+//      })
+//     .catch((err) => {
+//         console.log(err)
+//     });
+
+//     const x = [1,2,3,4];
+
+//     const [a,b] = x;
+//     console.log(a, b)
