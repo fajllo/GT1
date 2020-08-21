e69560aa7ace2c5c699c472ce228a1139c94c02d
fsdr <filipsordyl8@unilever.com>
2020-08-21 15:45:31 +0200

    fetch bez funkcji

--- JS/httpRequest/xml/fetch/app.js
+++ JS/httpRequest/xml/fetch/app.js
@@ -1,9 +1,35 @@
 // https://swapi.dev/api/planets/
-fetch("https://swapi.dev/api/planets/").then((response) =>{
-    response.json().then((data) => {
-        for (let planet of data.results)
-        console.log(planet.name)
+const fetchPlanets = (url= "https://swapi.dev/api/planets/") => {
+    return fetch(url)
+}
 
+
+fetch("https://swapi.dev/api/planets/")
+    .then((response) =>{
+        if (!response.ok){
+            throw new Error("wrrrrrrrrrr!");
+        }
+        return response.json();
     })
+    .then((data) => {
+        const film = data.results[0].films[0];
+        return fetch(film)
+     })
+     .then((response) => {
+        if (!response.ok){
+            throw new Error("wrrrrrrrrrr!");
+        }
+        return response.json();
 
-});
+     })
+     .then((data) => {
+         console.log(data.title);
+     })
+    .catch((err) => {
+        console.log(err)
+    });
+
+    const x = [1,2,3,4];
+
+    const [a,b] = x;
+    console.log(a, b)
