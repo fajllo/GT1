ab7ce5a9c0936d107b2e7950c208fb28806c111b
fsdr <filipsordyl8@unilever.com>
2020-08-23 15:36:42 +0200

    multpple await

---
+++ JS/async and await/app.js
@@ -0,0 +1,53 @@
+
+const btn = document.querySelector("button");
+
+const moveX = (element,amont,deley) => {
+	return new Promise((resolve, reject) =>{
+		setTimeout(()=>{
+			const bodyWidth = document.body.clientWidth;
+			const elRight = element.getBoundingClientRect().right;
+			const elLeft = element.getBoundingClientRect().left;
+			if(elRight + amont >bodyWidth){
+				reject();
+			}
+			else {
+				element.style.transform = `translateX(${elLeft + amont}px)`;
+				resolve();
+			}
+		},deley)
+
+	})
+};
+
+async function animateR() {
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+    await moveX(btn,100,500)
+
+}
+
+animateR()
