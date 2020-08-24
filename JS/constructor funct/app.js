433f80bc8ceb5d1c736aecdd2cc31b60d07ccc6a
fsdr <filipsordyl8@unilever.com>
2020-08-24 12:16:13 +0200

    koniec

--- JS/constructor funct/app.js
+++ JS/constructor funct/app.js
@@ -2,7 +2,6 @@ function Color(r,g,b){
     this.r = r;
     this.g = g;
     this.b = b;
-
 }
 
 Color.prototype.rgb = function(){
@@ -14,9 +13,9 @@ Color.prototype.hex = function() {
     return "#" +((1<<24) +(r<<16)+(g<<8)+b).toString(16).slice(1)
 }
 
-Color.prototype.rgba = function(a=1){
+Color.prototype.rgba = function(a=1.0){
     const {r,g,b} = this;
-    return `rgb(${r}, ${g}, ${b}, ${a})`;
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
 };
 const color1 = new Color(100,200,130);
 const color2 = new Color(0,0,0);
