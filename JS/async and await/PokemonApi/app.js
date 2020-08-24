bd4898d5640292cb37b450dc34dbf5e7a94daae0
fsdr <filipsordyl8@unilever.com>
2020-08-24 09:35:00 +0200

    parallel

--- JS/async and await/PokemonApi/app.js
+++ JS/async and await/PokemonApi/app.js
@@ -12,12 +12,12 @@
 
 //parallel request
 async function getPokemon(){
-    const poke1 =  axios.get("https://pokeapi.co/api/v2/pokemon/1")
-    const poke2 =  axios.get("https://pokeapi.co/api/v2/pokemon/2")
-    const poke3 =  axios.get("https://pokeapi.co/api/v2/pokemon/3")
-    await poke1;
-    await poke2;
-    await poke3;
+    const prom1 =  axios.get("https://pokeapi.co/api/v2/pokemon/1")
+    const prom2 =  axios.get("https://pokeapi.co/api/v2/pokemon/2")
+    const prom3 =  axios.get("https://pokeapi.co/api/v2/pokemon/3")
+    const poke1 = await prom1;
+    const poke2 = await prom2;
+    const poke3 = await prom3;
     console.log(poke1.data.name)
     console.log(poke2.data.name)
     console.log(poke3.data.name)
