e791b7817b9a06b4a967844a653c1355f8d51e43
fsdr <filipsordyl8@unilever.com>
2020-08-24 12:45:57 +0200

    hex method

--- JS/classe/app.js
+++ JS/classe/app.js
@@ -4,10 +4,15 @@ class Color{
         this.g = g;
         this.b = b;
         this.name = name;
+
     }
    rgb(){
         const {r,g,b} = this;
         return `rgb(${r}, ${g}, ${b})`;
+    };
+    hex(){
+        const {r,b,g} = this;
+        return "#" +((1<<24) +(r<<16)+(g<<8)+b).toString(16).slice(1)
     };
 }
 const c1 = new Color(100,255,10,"fifi");
