f428557227876d2a0ce443d6a1b71c5ab9174301
fsdr <filipsordyl8@unilever.com>
2020-08-24 12:10:50 +0200

    new hex i rgba

--- JS/constructor funct/app.js
+++ JS/constructor funct/app.js
@@ -9,3 +9,14 @@ Color.prototype.rgb = function(){
     const {r,g,b} = this;
     return `rgb(${r}, ${g}, ${b})`;
 };
+Color.prototype.hex = function() {
+    const {r,b,g} = this;
+    return "#" +((1<<24) +(r<<16)+(g<<8)+b).toString(16).slice(1)
+}
+
+Color.prototype.rgba = function(a=1){
+    const {r,g,b} = this;
+    return `rgb(${r}, ${g}, ${b}, ${a})`;
+};
+const color1 = new Color(100,200,130);
+const color2 = new Color(0,0,0);
