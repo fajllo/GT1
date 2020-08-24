9c0b8c192b9974ffd88b61123cfc208a4fc0f6fd
fsdr <filipsordyl8@unilever.com>
2020-08-24 09:30:41 +0200

    parralel request

--- JS/async and await/PokemonApi/app.js
+++ JS/async and await/PokemonApi/app.js
@@ -1,8 +1,23 @@
 //sequencial requset
+// async function getPokemon(){
+//     const poke1 = await axios.get("https://pokeapi.co/api/v2/pokemon/1")
+//     const poke2 = await axios.get("https://pokeapi.co/api/v2/pokemon/2")
+//     const poke3 = await axios.get("https://pokeapi.co/api/v2/pokemon/3")
+//     console.log(poke1.data.name)
+//     console.log(poke2.data.name)
+//     console.log(poke3.data.name)
+// }
+//  getPokemon()
+
+
+//parallel request
 async function getPokemon(){
-    const poke1 = await axios.get("https://pokeapi.co/api/v2/pokemon/1")
-    const poke2 = await axios.get("https://pokeapi.co/api/v2/pokemon/2")
-    const poke3 = await axios.get("https://pokeapi.co/api/v2/pokemon/3")
+    const poke1 =  axios.get("https://pokeapi.co/api/v2/pokemon/1")
+    const poke2 =  axios.get("https://pokeapi.co/api/v2/pokemon/2")
+    const poke3 =  axios.get("https://pokeapi.co/api/v2/pokemon/3")
+    await poke1;
+    await poke2;
+    await poke3;
     console.log(poke1.data.name)
     console.log(poke2.data.name)
     console.log(poke3.data.name)
