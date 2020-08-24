289e85b50e0845bf80602d5bb0b397d9955c0154
fsdr <filipsordyl8@unilever.com>
2020-08-24 09:41:01 +0200

    Promise .all

--- JS/async and await/PokemonApi/app.js
+++ JS/async and await/PokemonApi/app.js
@@ -15,11 +15,7 @@ async function getPokemon(){
     const prom1 =  axios.get("https://pokeapi.co/api/v2/pokemon/1")
     const prom2 =  axios.get("https://pokeapi.co/api/v2/pokemon/2")
     const prom3 =  axios.get("https://pokeapi.co/api/v2/pokemon/3")
-    const poke1 = await prom1;
-    const poke2 = await prom2;
-    const poke3 = await prom3;
-    console.log(poke1.data.name)
-    console.log(poke2.data.name)
-    console.log(poke3.data.name)
+    const result = await Promise.all([prom1,prom2,prom3])
+    console.log(result)
 }
  getPokemon()
